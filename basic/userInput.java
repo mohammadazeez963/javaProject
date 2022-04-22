7b1d8029a9b799191798f73920a0445c4b017ec1
Mohammad Azeez <m.azeez@zensar.com>
2022-04-22 16:17:24 +0530

    test

---
+++ basic/userInput.java
@@ -0,0 +1,21 @@
+package basic;
+
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // // System.out.println("Enter a number");
+        // // int n = sc.nextInt();
+        // System.out.println(" Enter any Name");
+        // double n = sc.nextInt();
+        // System.out.println("Your name is "+n);
+        int a;
+         a = 6;
+         System.out.println(a);
+        
+        
+       
+    }
+    
+}
