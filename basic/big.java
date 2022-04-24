391568014c355934226807314260e81017d685f0
Mohammad Azeez <m.azeez@zensar.com>
2022-04-24 10:59:42 +0530

    hello

---
+++ basic/big.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class big {
+    public static void main(String[] args) {
+        int a= 13;
+        if(a>18){
+            System.out.println(a+" is big");}
+            else System.out.println(a+" is small");
+        }
+    }
+    
+
